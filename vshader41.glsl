53982d69ce516217abba2d3ac197530eaeecdbfa
William Thibault <tebo@sn336s36.sci>
2012-10-16 08:41:10 -0700

    removed perspective divide and changed names

--- vshader41.glsl
+++ vshader41.glsl
@@ -2,11 +2,11 @@ attribute  vec4 vPosition;
 attribute  vec4 vColor;
 varying vec4 color;
 
-uniform mat4 model_view;
-uniform mat4 projection;
+uniform mat4 modelviewMatrix;
+uniform mat4 projectionMatrix;
 
 void main() 
 {
-    gl_Position = projection*model_view*vPosition/vPosition.w;
+    gl_Position = projectionMatrix*modelviewMatrix*vPosition;
     color = vColor;
 } 
